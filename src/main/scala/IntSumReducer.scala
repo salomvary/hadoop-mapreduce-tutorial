6e4f3b48c707af60b5f8b1980b9ac8e24e22b9d0
Márton Salomváry <salomvary@gmail.com>
2017-08-24 16:29:52 +0200

    Inline some code in mapper/reducer

--- src/main/scala/IntSumReducer.scala
+++ src/main/scala/IntSumReducer.scala
@@ -1,18 +1,18 @@
 import java.lang.Iterable
 
-import org.apache.hadoop.io.{IntWritable, Text}
-import org.apache.hadoop.mapreduce.Reducer
+import org.apache.hadoop.io._
+import org.apache.hadoop.mapreduce._
 
 import scala.collection.JavaConverters._
 
-class IntSumReducer extends Reducer[Text, IntWritable, Text, IntWritable] {
-  private val result = new IntWritable
+class IntSumReducer
+  extends Reducer[Text, IntWritable, Text, IntWritable] {
 
   override def reduce(key: Text,
                       values: Iterable[IntWritable],
                       context: Reducer[Text, IntWritable, Text, IntWritable]#Context) {
     val sum = values.asScala.map(_.get()).sum
-    result.set(sum)
-    context.write(key, result)
+    val value = new IntWritable(sum)
+    context.write(key, value)
   }
 }
--- src/main/scala/TokenizerMapper.scala
+++ src/main/scala/TokenizerMapper.scala
@@ -1,16 +1,20 @@
-import org.apache.hadoop.io.{IntWritable, Text}
-import org.apache.hadoop.mapreduce.Mapper
+import org.apache.hadoop.io._
+import org.apache.hadoop.mapreduce._
 
-class TokenizerMapper extends Mapper[IntWritable, Text, Text, IntWritable] {
-  private val one = new IntWritable(1)
-  private val word = new Text
+class TokenizerMapper
+  extends Mapper[IntWritable, Text, Text, IntWritable] {
 
   override def map(key: IntWritable,
                    value: Text,
                    context: Mapper[IntWritable, Text, Text, IntWritable]#Context) {
-    value.toString.split("\\s+").foreach { nextWord =>
-      word.set(nextWord)
-      context.write(word, one)
+
+    val words = value.toString.split("\\s+")
+
+    words.foreach { word =>
+      val key = new Text(word)
+      val value = new IntWritable(1)
+      context.write(key, value)
     }
+
   }
 }
